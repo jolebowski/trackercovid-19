4038af7b806751c5768be95ddd60dae3dacb2a6b
jolebowski <jolebowski@gmail.com>
2020-05-10 11:53:10 +0200

    edit files for the scroll of countries

--- src/components/CountryPicker/index.js
+++ src/components/CountryPicker/index.js
@@ -41,58 +41,59 @@ const CountryPicker = ({
 
   return (
     <div className={styles.tabCountry}>
-      <div className={styles.info}>
-        <div className={styles.areas}>
-          <div className={styles.searchCountrie}>
-            <input
-              type="text"
-              placeholder="Entrez le nom d'un pays"
-              autocapitalize="off"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <div className={styles.countrieContent}>
-              {search !== "" &&
-                filteredCountries &&
-                filteredCountries.map((country) => (
-                  <div
-                    className={cx(styles.country)}
-                    onClick={(e) =>
-                      handleCountryChange(e.currentTarget.dataset.country)
-                    }
-                    data-country={country}
-                  >
-                    {country}
-                  </div>
-                ))}
-            </div>
-          </div>
-          {fetchedCountries &&
-            fetchedCountries.map((country, index) => (
-              <div id={country} className={styles.divArea}>
+      <div className={styles.areas}>
+        <div className={styles.searchCountrie}>
+          <input
+            type="text"
+            placeholder="Entrez le nom d'un pays"
+            autocapitalize="off"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <div className={styles.countrieContent}>
+            {search !== "" &&
+              filteredCountries &&
+              filteredCountries.map((country) => (
                 <div
-                  className={cx(
-                    styles.country,
-                    addClass === index && styles.active
-                  )}
-                  key={index}
+                  className={cx(styles.country)}
                   onClick={(e) =>
-                    handleCountryChange(e.currentTarget.dataset.country, index)
+                    handleCountryChange(e.currentTarget.dataset.country)
                   }
                   data-country={country}
                 >
-                  <div className={styles.areaName}>{country}</div>
-                  <div className={styles.areaTotal}>
-                    <div className={styles.secondaryInfo}>
-                      {total.map(
-                        (to) => to.combinedKey === country && to.confirmed.toString()
-                        .replace(/\B(?=(?:\d{3})+(?!\d))/g, ",")
-                      )}
-                    </div>
+                  {country}
+                </div>
+              ))}
+          </div>
+        </div>
+        {fetchedCountries &&
+          fetchedCountries.map((country, index) => (
+            <div id={country} className={styles.divArea}>
+              <div
+                className={cx(
+                  styles.country,
+                  addClass === index && styles.active
+                )}
+                key={index}
+                onClick={(e) =>
+                  handleCountryChange(e.currentTarget.dataset.country, index)
+                }
+                data-country={country}
+              >
+                <div className={styles.areaName}>{country}</div>
+                <div className={styles.areaTotal}>
+                  <div className={styles.secondaryInfo}>
+                    {total.map(
+                      (to) =>
+                        to.combinedKey === country &&
+                        to.confirmed
+                          .toString()
+                          .replace(/\B(?=(?:\d{3})+(?!\d))/g, ",")
+                    )}
                   </div>
                 </div>
               </div>
-            ))}
-        </div>
+            </div>
+          ))}
       </div>
     </div>
   );
